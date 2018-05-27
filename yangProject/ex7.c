a0365cd27ddea87fef07673b1efeae3a18635df9
sail <sailcpu@gmail.com>
2018-05-27 22:49:34 +0800

    ex7

---
+++ yangProject/ex7.c
@@ -0,0 +1,23 @@
+//
+// Created by top on 18-5-27.
+//
+
+//从键盘输入一个数，判断是否是素数
+#include <stdio.h>
+void main() {
+    int a,b,c;
+    scanf("%d",&a);
+
+    for(b=2;b<a;b++){
+        if (a%b==0){
+            c++;
+        }
+    }
+        if(c>0){
+            printf("不是质数");
+        } else{
+            printf("是质数");
+        }
+
+
+}
---
+++ yangProject/ex8.c
@@ -0,0 +1,14 @@
+//
+// Created by top on 18-5-27.
+//
+//求1+1/3+1/5+...+1/99
+#include <stdio.h>
+void main() {
+    int a,b,s;
+        a=0;
+        b = 1 + 2*a;
+        s =1/b+s;
+        printf("1+1/3+1/5+...+1/99=%d",s);
+
+
+}
