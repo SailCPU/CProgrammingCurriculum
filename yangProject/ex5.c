7900d7d21a82aaad47356bfe38d21f91094679a7
sail <sailcpu@gmail.com>
2018-05-27 22:02:01 +0800

    ex4

---
+++ yangProject/ex5.c
@@ -0,0 +1,12 @@
+//
+// Created by top on 18-5-27.
+//
+//从键盘输入一个整数，输出其绝对值
+#include <stdio.h>
+#include <math.h>
+void main(){
+    int a,b;
+    scanf("%d",&a);
+    b=fabs(a);
+    printf("%d",b);
+}
