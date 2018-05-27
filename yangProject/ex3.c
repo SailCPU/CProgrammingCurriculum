af2682563b654d7fe2fbb17c2ddefdea4d005745
sail <sailcpu@gmail.com>
2018-05-27 21:35:41 +0800

    ex3

---
+++ yangProject/ex3.c
@@ -0,0 +1,14 @@
+//
+// Created by top on 18-5-27.
+//
+//用户输入矩形的长,宽,输出面积（如长为6.5,宽为4.5）
+#include <stdio.h>
+void main() {
+    float a,b,s;
+    scanf("%f,%f",&a,&b);
+    s=a*b;
+    printf("s=%f\n",s);
+}
+
+
+
