e629516c6648e112a2ec893450cf8926a241748b
sail <sailcpu@gmail.com>
2018-05-27 22:58:03 +0800

    ex8

--- yangProject/ex8.c
+++ yangProject/ex8.c
@@ -4,11 +4,12 @@
 //求1+1/3+1/5+...+1/99
 #include <stdio.h>
 void main() {
-    int a,b,s;
-        a=0;
-        b = 1 + 2*a;
-        s =1/b+s;
-        printf("1+1/3+1/5+...+1/99=%d",s);
+    float a,b,s;
+        for(a=0;a<50;a++){
+            b=1/(1+2*a);
+            s=b+s;
+        }
+        printf("1+1/3+1/5+...+1/99=%f",s);
 
 
 }
