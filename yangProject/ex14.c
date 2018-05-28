0498a91e6f50dda04a0dc8886d3ba3fe8da8cebe
sail <sailcpu@gmail.com>
2018-05-28 23:00:29 +0800

    ex14

--- yangProject/ex14.c
+++ yangProject/ex14.c
@@ -5,5 +5,14 @@
 */
 #include <stdio.h>
 void main(){
+    int i,j,a;
+    for(i=1;i<10;i++){
+        for(j=1;j<10;j++){
+            a=i*j;
+            printf("%d*%d=%d    ",i,j,a);
+        }printf("\n");
+    }
+
+
     printf("ex14");
 }
