104380f26544ca1774ffc176f24f286d90a32907
sail <sailcpu@gmail.com>
2018-05-31 19:34:48 +0800

    ex21

--- yangProject/ex21.c
+++ yangProject/ex21.c
@@ -3,7 +3,16 @@
 1.程序分析：利用辗除法。
 
 */
+
 #include <stdio.h>
-void main(){
-    printf("ex21");
+
+void main() {
+    int m, n, a, b, c;
+    scanf("%d%d", &a, &b);
+    m=a;   n=b;
+    while(b!=0)
+    { c=a%b; a=b;  b=c;}
+    printf("最大公约数:%d\n", a);
+    printf("最小公倍数:%d\n", m*n/a);
 }
+
