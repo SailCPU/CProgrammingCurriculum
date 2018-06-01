057515ba51bf7974efd5f918c8155b4a589760b0
sail <sailcpu@gmail.com>
2018-06-02 00:19:25 +0800

    ex25

--- yangProject/ex25.c
+++ yangProject/ex25.c
@@ -4,5 +4,15 @@
 */
 #include <stdio.h>
 void main(){
-    printf("ex25");
+    float a,c,s,h;
+    int b;
+    a=100,s=100;
+    scanf("%d",&b);
+    for (c=2;c<=b;c++) {
+        h=a/2;
+        a=h;
+        s=s+h;
+
+    }
+    printf("第%d次落地时，共经过%f米，第%d次反弹%f米",b,s,b,h);
 }
